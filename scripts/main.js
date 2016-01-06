271ec67b3af1c87d7d7f1418ed7d41d8731f3572
Whitney Murray <wnksmith92@yahoo.com>
2016-01-05 18:38:04 -0600

    made prompt

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,4 @@
 'use strict';
+var name = prompt('Hello what word should I refer to you as?');
+var greet = 'Hello' + ' ' + name +' ' + 'your hair is really on point today.' + 'I hope you are fairing well.';
+console.log(greet);
