de041bd3364c84e20e22a99f48526b4a74b13d82
Whitney Murray <wnksmith92@yahoo.com>
2016-01-05 19:15:57 -0600

    finished name exercise

--- scripts/main.js
+++ scripts/main.js
@@ -2,3 +2,7 @@
 var name = prompt('Hello what word should I refer to you as?');
 var greet = 'Hello' + ' ' + name +' ' + 'your hair is really on point today.' + 'I hope you are fairing well.';
 console.log(greet);
+var list = document.getElementById('list');
+list.innerHTML = greet;
+
+var sent = 'She"\'"s going to the park';
